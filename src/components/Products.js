f20eaeabfbca72cf62e07156cf665d31c4a26cf7
KenetC <kenetchape@gmail.com>
2022-03-27 17:00:52 -0300

    Kenet - cambios en ProductList

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import {Link, Route, Switch,NavLink} from 'react-router-dom';
 import ProdDetail from './prodDetail';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Products(){
 	const [products, setproducts] = useState({data:"Data Default"});
@@ -14,68 +15,19 @@ function Products(){
 			.catch(error => console.log(error))
 	}, [])
     return(
-        <div className='container'>
-            <table class="table  table-bordered table-danger table-hover table-sm">
-                <thead>
-                    <tr>
-                    <th scope="col">ID</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Price</th>
-                    <th scope="col">Categoria</th>
-                    <th scope="col">Style</th>
-                    <th scope="col">Colours</th>
-                    <th scope="col">Sizes</th>
-                    <th scope="col">Detalle</th>
-                    </tr>
-                </thead>
-                <tbody>
-                { products.length > 0 && products.map(prod=>{
-                    let to = '/products/'+ prod.id
-                    return(
-                            <tr>
-                                <th scope="row">{prod.id}</th>
-                                <td>{prod.name}</td>
-                                <td>{prod.price}</td>
-                                <td>{prod.category}</td>
-                                <td>{prod.style}</td>
-                                <td>
-                                    {
-                                    prod.color.length > 0 && prod.color.map(color=>{
-                                            return(
-                                                <li className='list-unstyled'>{color}</li>
-                                            )
-                                        })
-                                    }
-                                </td>
-                                <td>
-                                {
-                                    prod.size.length > 0 && prod.size.map(size=>{
-                                        return(
-                                            <li className='list-unstyled'>{size}</li>
-                                        )
-                                    })
-                                }
-                                </td>
-                                <td><Link to={to}><span>Detalle de producto</span></Link></td>
-                            </tr>
-                            
-                    )
-                
-                })
-            }
-            </tbody>
-            </table>
-        <Switch>
-        {
-            products.length > 0 && products.map(prod=>{
-                let to = `/products/${prod.id}}`;
-                return(
-                    <Route path={to}><ProdDetail/></Route>
-                )
-            })
-        }
-        </Switch>
-        </div> 
+        <React.Fragment>
+        <h1>Total de usuarios: {products.length}</h1>
+        <div className='container-fluid'>
+            <div class="card" >
+            <img src="..." class="card-img-top"></img>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="/" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        </React.Fragment>
     )
 }
 
